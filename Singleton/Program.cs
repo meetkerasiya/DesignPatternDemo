fd87bc92770d74167daa57bab4a66c5474da11b0
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-03-10 15:23:29 +0530

    First Commit

---
+++ Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatternDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Singleton obj1 = Singleton.GetObject();
+            obj1.TestMethod();
+
+            Singleton obj2 = Singleton.GetObject();
+            obj2.TestMethod();
+
+
+        }
+    }
+
+
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatternDemo
+{
+    sealed class Singleton
+    {
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton getinstance = null;
+        public static Singleton GetObject()
+        {
+            if(getinstance==null)
+            {
+                return new Singleton();
+
+            }
+            return getinstance;
+        }
+
+        public void TestMethod()
+        {
+            Console.WriteLine("This is a test method");
+        }
+    }
+}
