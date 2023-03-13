530271e6f008d309d5c49903f1ae98829315f9fe
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-03-13 17:48:55 +0530

    Chain of responsibility pattern added

---
+++ ChainOfRespnsibilityDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using ChainOfRespnsibilityDemo;
+
+RequestHandler rh1 = new Handler();
+RequestHandler rh2 = new BiggerHandler();
+RequestHandler rh3 = new BiggestHandler();
+
+rh1.SetSuccessor(rh2);
+rh2.SetSuccessor(rh3);
+
+int[] requests = { 12, 8, 6, 22, 18, 44, 35 };
+
+foreach(int request in requests)
+{
+    rh1.HandleRequest(request);
+}
---
+++ ChainOfRespnsibilityDemo/RequestHandlers.cs
@@ -0,0 +1,65 @@
+﻿namespace ChainOfRespnsibilityDemo
+{
+    public abstract class RequestHandler
+    {
+        protected RequestHandler successor;
+
+        public void SetSuccessor(RequestHandler successor)
+        {
+            this.successor = successor;
+        }
+
+        public abstract void HandleRequest(int number_of_requests);
+    }
+
+    public class Handler : RequestHandler
+    {
+        public override void HandleRequest(int number_of_requests)
+        {
+            if(number_of_requests>=0 && number_of_requests<=10)
+            {
+                Console.WriteLine($"{this.GetType().Name} handled the {number_of_requests} requests");
+
+            }
+            else if(successor!=null)
+            {
+                successor.HandleRequest(number_of_requests);
+            }
+        }
+    }
+
+    public class BiggerHandler : RequestHandler
+    {
+        public override void HandleRequest(int number_of_requests)
+        {
+            if(number_of_requests>=11 && number_of_requests<=20)
+            {
+                Console.WriteLine($"{this.GetType().Name} handled the {number_of_requests} requests");
+
+            }
+            else if(successor!=null)
+            {
+                successor.HandleRequest(number_of_requests);
+            }
+        }
+    }
+
+    public class BiggestHandler : RequestHandler
+    {
+        public override void HandleRequest(int number_of_requests)
+        {
+            if(number_of_requests>=21 && number_of_requests<=30)
+            {
+                Console.WriteLine($"{this.GetType().Name} handled the {number_of_requests} requests");
+
+            }
+            else if(successor!=null)
+            {
+                successor.HandleRequest(number_of_requests);
+            }
+        }
+    }
+
+
+
+}
