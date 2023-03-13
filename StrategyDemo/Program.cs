dc9510d85e42de1e2f22cd2bb011c97570434f2a
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-03-13 17:34:43 +0530

    Strategy pattern added

---
+++ StrategyDemo/Program.cs
@@ -0,0 +1,6 @@
+﻿using StrategyDemo;
+
+var chainTasks = new Log().Append(new Transaction()).Append(new Add(15));
+
+chainTasks.Append(new Transaction()).Append(new Add(32));
+chainTasks.Do(5);
---
+++ StrategyDemo/Strategy.cs
@@ -0,0 +1,62 @@
+﻿namespace StrategyDemo
+{
+    public abstract class Strategy
+    {
+        Strategy _next;
+        public Strategy Append(Strategy next)
+        {
+            if(_next == null)
+            {
+                _next = next;
+            }
+            else
+            {
+                _next.Append(next);
+            }
+            return this;
+        }
+
+        public abstract void Do(int n);
+
+        protected void Next(int n)
+        {
+            _next?.Do(n);
+        }
+    }
+
+    public class Log : Strategy
+    {
+        public override void Do(int n)
+        {
+            Console.WriteLine($"Logging a number {n}");
+            Next(n);
+        }
+    }
+    public class Add : Strategy
+    {
+        int _num;
+        public Add(int num)
+        {
+            _num = num;
+        }
+
+        public override void Do(int n)
+        {
+            Console.WriteLine($"Adding {_num} to {n}");
+            int sum = n += _num;
+            Console.WriteLine($"Sum={sum}");
+            Next(sum);
+        }
+    }
+
+    public class Transaction : Strategy
+    {
+
+        public override void Do(int n)
+        {
+            Console.WriteLine("Starting transaction");
+            Next(n);
+            Console.WriteLine("Finishing transaction");
+        }
+    }
+}
